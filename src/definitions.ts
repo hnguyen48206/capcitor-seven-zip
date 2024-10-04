ba5aae33957d69a2a7ef16f3d58a94c2eba980f1
hnguyen48206 <hoangbich1989@gmail.com>
2024-10-04 11:00:14 +0700

    abc

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,6 +1,9 @@
 export interface SevenzipPlugin {
   unzip(options: SevenzipOtions): Promise<boolean>;
 }
+//password và outputDir là optional. Mặc định, file giải nén sẽ lưu ở thư mục Document của App (trên iOS))
+//nếu truyền outputDir thì sẽ là subpath của Document, ví dụ '/subthumuc/thumuc1'
+//lưu ý là subDir này cần tạo trước và bảo đảm có tồn tại trước khi truyền vào unzip
 export interface SevenzipOtions {
   fileURL: string;
   password?: string;
